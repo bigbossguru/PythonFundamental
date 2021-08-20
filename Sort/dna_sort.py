cfe92736ecdd8deaccff28c42213d6a0363a0337
Eldar Gazizov <egazizov@elektroline.cz>
2021-08-20 10:56:43 +0200

    refactoring sorting generic added new functions

--- Sort/dna_sort.py
+++ Sort/dna_sort.py
@@ -1,4 +1,4 @@
-from generic_sort import bubble_sort
+from generic_sort import insertion_sort, selection_sort
 from typing import List
 
 
@@ -6,4 +6,10 @@ if __name__ == "__main__":
     list_of_numbers: List[int] = [3,7,2,1,5,4]
     original_list_of_codons: List[str] = ['CGG', 'AAC', 'ATC', 'CTT', 'GAA', 'GCT', 'TCG', 'GTA', 'ACC', 'AGG', 'GTA', 'GTT']
 
-    print("Original Genom: ", original_list_of_codons)
+    print("Original Genom:\t\t\t", original_list_of_codons)
+    print("Standart sort in Python:\t", sorted(original_list_of_codons))
+    print("Original Genom:\t\t\t", original_list_of_codons)
+    print("Insertion sort custom:\t\t", insertion_sort(original_list_of_codons))
+    print("Original Genom:\t\t\t", original_list_of_codons)
+    print("Selection sort custom:\t\t", selection_sort(original_list_of_codons))
+    print("Original Genom:\t\t\t", original_list_of_codons)
--- Sort/generic_sort.py
+++ Sort/generic_sort.py
@@ -3,14 +3,48 @@ from copy import copy
 
 T = TypeVar('T')
 
+def merge_sort(container: Iterable[T]) -> Iterable[T]:
+    """ Merge Sort Algorithm """
+    pass
+
+def selection_sort(container: Iterable[T]) -> Iterable[T]:
+    """ Selection Sort Algorithm """
+
+    length: int = len(container)
+    sorted: Iterable[T] = copy(container)
+
+    for i in range(length):
+        min_idx: int = i
+        for j in range(i+1, length):
+            if sorted[j] < sorted[min_idx]:
+                min_idx = j
+        sorted[i], sorted[min_idx] = sorted[min_idx], sorted[i]
+    return sorted
+
+def insertion_sort(container: Iterable[T]) -> Iterable[T]:
+    """ Insertion Sort Algorithm """
+
+    length: int = len(container)
+    sorted: Iterable[T] = copy(container)
+
+    for i in range(1, length):
+        key: T = sorted[i]
+        j: int = i-1
+
+        while j>=0 and key<sorted[j]:
+            sorted[j+1] = sorted[j]
+            j -= 1
+        sorted[j+1] = key
+    return sorted
+
 def bubble_sort(container: Iterable[T]) -> Iterable[T]:
     """ Bubble Sort Algorithm """
 
-    l: int = len(container)
+    length: int = len(container)
     sorted: Iterable[T] = copy(container)
 
-    for i in range(l-1):
-        for j in range(l-1-i):
+    for i in range(length-1):
+        for j in range(length-1-i):
             if sorted[j] > sorted[j+1]:
                 sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
     return sorted
