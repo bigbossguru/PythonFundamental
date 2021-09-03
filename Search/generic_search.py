8c9a63aa8cbb242e7584754231059a161a26720b
Eldar Gazizov <egazizov@elektroline.cz>
2021-09-03 15:22:06 +0200

    fixed bug on dfs algo, added random coords

--- Search/generic_search.py
+++ Search/generic_search.py
@@ -5,20 +5,20 @@ T = TypeVar('T')
 
 class Stack(Generic[T]):
     def __init__(self) -> None:
-        self.__container: List[T] = []
+        self._container: List[T] = []
     
     @property
     def empty(self) -> bool:
-        return not self.__container
+        return not self._container
     
     def push(self, item: T) -> None:
-        self.__container.append(item)
+        self._container.append(item)
     
     def pop(self) -> T:
-        return self.__container.pop()
+        return self._container.pop()
     
     def __repr__(self) -> str:
-        return repr(self.__container)
+        return repr(self._container)
 
 class Node(Generic[T]):
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
@@ -37,15 +37,15 @@ def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
 
     while not frontier.empty:
         current_node: Node[T] = frontier.pop()
-    current_state: T = current_node.state
+        current_state: T = current_node.state
 
-    if goal_test(current_state):
-        return current_node
-    
-    for child in successors(current_state):
-        if child in explored: continue
-        explored.add(child)
-        frontier.push(Node(child, current_node))
+        if goal_test(current_state):
+            return current_node
+        
+        for child in successors(current_state):
+            if child in explored: continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
     return None
 
 def node_to_path(node: Node[T]) -> List[T]:
--- Search/labyrinth.py
+++ Search/labyrinth.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import List, NamedTuple, Optional
 from random import uniform
 from generic_search import Node, node_to_path, dfs
+from random import randint
 
 class Cell(str, Enum):
     EMPTY = " "
@@ -15,67 +16,69 @@ class LabyrinthLocation(NamedTuple):
     column: int
 
 class Labyrinth:
-    def __init__(self, rows: int = 10, columns: int = 10, rarity: float = 0.05,
+    def __init__(self, rows: int = 10, columns: int = 10, rarity: float = 0.2,
                 start: LabyrinthLocation = LabyrinthLocation(0,0), 
                 goal: LabyrinthLocation = LabyrinthLocation(9,9)) -> None:
-        self.__rows = rows
-        self.__columns = columns
+        self._rows = rows
+        self._columns = columns
         self.start = start
         self.goal = goal
-        self.__grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
-        self.__randomly_fill(rows, columns, rarity)
-        self.__grid[start.row][start.column] = Cell.START
-        self.__grid[goal.row][goal.column] = Cell.GOAL
+        self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
+        self._randomly_fill(rows, columns, rarity)
+        self._grid[start.row][start.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
     
-    def __randomly_fill(self, rows: int, columns: int, rarity: float) -> None:
+    def _randomly_fill(self, rows: int, columns: int, rarity: float) -> None:
         for row in range(rows):
             for column in range(columns):
                 if uniform(0, 1.0) < rarity:
-                    self.__grid[row][column] = Cell.BLOCKED
+                    self._grid[row][column] = Cell.BLOCKED
     
     def goal_test(self, labloc: LabyrinthLocation) -> bool:
         return labloc == self.goal
 
     def successors(self, labloc: LabyrinthLocation) -> List[LabyrinthLocation]:
         locations: List[LabyrinthLocation] = []
-        if labloc.row+1 < self.__rows and self.__grid[labloc.row+1][labloc.column] != Cell.BLOCKED:
+        if labloc.row+1 < self._rows and self._grid[labloc.row+1][labloc.column] != Cell.BLOCKED:
             locations.append(LabyrinthLocation(labloc.row+1, labloc.column))
-        if labloc.row-1 >= 0 and self.__grid[labloc.row-1][labloc.column] != Cell.BLOCKED:
+        if labloc.row-1 >= 0 and self._grid[labloc.row-1][labloc.column] != Cell.BLOCKED:
             locations.append(LabyrinthLocation(labloc.row-1, labloc.column))
-        if labloc.column+1 < self.__columns and self.__grid[labloc.row][labloc.column+1] != Cell.BLOCKED:
+        if labloc.column+1 < self._columns and self._grid[labloc.row][labloc.column+1] != Cell.BLOCKED:
             locations.append(LabyrinthLocation(labloc.row, labloc.column+1))
-        if labloc.column-1 >= 0 and self.__grid[labloc.row][labloc.column-1] != Cell.BLOCKED:
+        if labloc.column-1 >= 0 and self._grid[labloc.row][labloc.column-1] != Cell.BLOCKED:
             locations.append(LabyrinthLocation(labloc.row, labloc.column-1))
         return locations
     
     def mark(self, path: List[LabyrinthLocation]) -> None:
         for labloc in path:
-            self.__grid[labloc.row][labloc.column] =  Cell.PATH
-        self.__grid[self.start.row][self.start.column] = Cell.START
-        self.__grid[self.goal.row][self.goal.column] = Cell.GOAL
+            self._grid[labloc.row][labloc.column] =  Cell.PATH
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
     
     def clear(self, path: List[LabyrinthLocation]):
         for labloc in path:
-            self.__grid[labloc.row][labloc.column] =  Cell.EMPTY
-        self.__grid[self.start.row][self.start.column] = Cell.START
-        self.__grid[self.goal.row][self.goal.column] = Cell.GOAL
+            self._grid[labloc.row][labloc.column] =  Cell.EMPTY
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
     
     def __str__(self) -> str:
         output: str = ""
-        for row in self.__grid:
+        for row in self._grid:
             output += "".join([c.value for c in row]) + '\n'
         return output
 
 if __name__ == "__main__":
-    labyrinth: Labyrinth = Labyrinth()
-    print(labyrinth)
+    coords = [(randint(0,20), randint(0,60)) for _ in range(2)]
+    labyrinth: Labyrinth = Labyrinth(rows=20, columns=60, start=LabyrinthLocation(*coords[0]), goal=LabyrinthLocation(*coords[1]), rarity=0.2)
+    # print(labyrinth)
 
     solution1: Optional[Node[LabyrinthLocation]] = dfs(labyrinth.start, labyrinth.goal_test, labyrinth.successors)
 
     if solution1 is None:
-        print("No solution found using depth-first search!")
+        print("\tNo solution found using depth-first search!\n".upper())
     else:
         path1: List[LabyrinthLocation] = node_to_path(solution1)
         labyrinth.mark(path1)
         print(labyrinth)
         labyrinth.clear(path1)
+        print("\tGreat, we have path to our goal!\n".upper())
