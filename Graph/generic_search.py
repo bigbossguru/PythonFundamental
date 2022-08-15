8967c268f8917c0ff0a8d6e7f4a8ec2f0b418e33
Eldar Gazizov <kursus1010@gmail.com>
2022-08-15 17:04:56 +0200

    Graphs added BFS and DFS Algorithms

---
+++ Graph/generic_search.py
@@ -0,0 +1,62 @@
+from typing import Hashable
+
+
+def breadth_first_search(graph: dict, src: str, dst: str) -> bool:
+    """Breadth First Search Algorithm
+
+    :param dict graph: _description_
+    :param str src: _description_
+    :param str dst: _description_
+    :return bool: _description_
+    """
+
+    visited = []
+    queue = [src]
+
+    while len(queue) > 0:
+        current_node = queue.pop(0)
+        visited.append(current_node)
+
+        if current_node == dst:
+            return visited
+
+        for node in graph.get(current_node):
+            queue.append(node)
+
+
+def depth_first_search(graph: dict, src: str, dst: str) -> bool:
+    """Depth First Search Algorithm
+
+    :param dict graph: _description_
+    :param str src: _description_
+    :param str dst: _description_
+    :return bool: _description_
+    """
+
+    visited = []
+    stack = [src]
+
+    while len(stack) > 0:
+        current_node = stack.pop()
+        visited.append(current_node)
+
+        if current_node == dst:
+            return visited
+
+        for node in graph.get(current_node):
+            stack.append(node)
+
+
+if __name__ == "__main__":
+    graph = {
+        "a": ["b", "c"],
+        "b": ["d"],
+        "c": ["e", "j"],
+        "e": ["b"],
+        "d": [],
+        "f": ["d"],
+        "j": [],
+    }
+
+    print(breadth_first_search(graph, "a", "d"))
+    print(depth_first_search(graph, "a", "d"))
--- Sort/generic_sort.py
+++ Sort/generic_sort.py
@@ -1,11 +1,12 @@
 from typing import Iterable, TypeVar
 from copy import copy
 
-T = TypeVar('T')
+T = TypeVar("T")
+
 
 def merge_sort(container: Iterable[T]) -> Iterable[T]:
-    """ Merge Sort Algorithm """
-    
+    """Merge Sort Algorithm"""
+
     length: int = len(container)
     sorted: int = copy(container)
 
@@ -29,58 +30,61 @@ def merge_sort(container: Iterable[T]) -> Iterable[T]:
             sorted[k] = left[i]
             i += 1
             k += 1
-            
+
         while j < len(right):
             sorted[k] = right[j]
             j += 1
             k += 1
     return sorted
 
+
 def selection_sort(container: Iterable[T]) -> Iterable[T]:
-    """ Selection Sort Algorithm """
+    """Selection Sort Algorithm"""
 
     length: int = len(container)
     sorted: Iterable[T] = copy(container)
 
     for i in range(length):
         min_idx: int = i
-        for j in range(i+1, length):
+        for j in range(i + 1, length):
             if sorted[j] < sorted[min_idx]:
                 min_idx = j
         sorted[i], sorted[min_idx] = sorted[min_idx], sorted[i]
     return sorted
 
+
 def insertion_sort(container: Iterable[T]) -> Iterable[T]:
-    """ Insertion Sort Algorithm """
+    """Insertion Sort Algorithm"""
 
     length: int = len(container)
     sorted: Iterable[T] = copy(container)
 
     for i in range(1, length):
         key: T = sorted[i]
-        j: int = i-1
+        j: int = i - 1
 
-        while j>=0 and key<sorted[j]:
-            sorted[j+1] = sorted[j]
+        while j >= 0 and key < sorted[j]:
+            sorted[j + 1] = sorted[j]
             j -= 1
-        sorted[j+1] = key
+        sorted[j + 1] = key
     return sorted
 
+
 def bubble_sort(container: Iterable[T]) -> Iterable[T]:
-    """ Bubble Sort Algorithm """
+    """Bubble Sort Algorithm"""
 
     length: int = len(container)
     sorted: Iterable[T] = copy(container)
 
-    for i in range(length-1):
-        for j in range(length-1-i):
-            if sorted[j] > sorted[j+1]:
-                sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
+    for i in range(length - 1):
+        for j in range(length - 1 - i):
+            if sorted[j] > sorted[j + 1]:
+                sorted[j], sorted[j + 1] = sorted[j + 1], sorted[j]
     return sorted
 
 
 def quick_sort(container: Iterable[T]) -> Iterable[T]:
-    """ Quick Sort Algorithm """
+    """Quick Sort Algorithm"""
 
     length: int = len(container)
     sorted: Iterable[T] = copy(container)
@@ -97,17 +101,14 @@ def quick_sort(container: Iterable[T]) -> Iterable[T]:
                 if container[j] <= pivot:
                     i += 1
                     container[i], container[j] = container[j], container[i]
-            container[i+1], container[right] = container[right], container[i+1]
-            return i+1
-        
+            container[i + 1], container[right] = container[right], container[i + 1]
+            return i + 1
+
         if left < right:
             part_idx = partition(container, left, right)
-            _qs(container, left, part_idx-1)
-            _qs(container, part_idx+1, right)
+            _qs(container, left, part_idx - 1)
+            _qs(container, part_idx + 1, right)
 
     _qs(sorted, left, right)
 
     return sorted
-
-
-
